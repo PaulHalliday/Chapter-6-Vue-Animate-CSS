562836bec6e87ebeb6067e65888c417c644225a3
PaulHalliday <hello@paulhalliday.io>
2017-12-22 02:39:14 +0000

    Added transitions.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1 class="animated fadeIn">Hello Vue!</h1>
+ <div id="app">
+  <transition name="fadeIn"
+    enter-active-class="animated fadeIn">
+    <h1 v-if="toggle">I fade in!</h1>
+  </transition>
+  <button @click="toggle = !toggle">Toggle Heading</button>
+ </div> 
 </template>
+
+<script>
+export default {
+ data () {
+  return {
+   toggle: false
+  }
+ }
+}
+</script>
